6688c979a67ed7d3d5ee601eb3d32c8b6f37d334
TheoHanon <99546301+TheoHanon@users.noreply.github.com>
2025-02-20 11:25:53 +0100

    Bump version to 0.1.4 in setup.py and __init__.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="spherical_inr",
-    version="0.1.3",
+    version="0.1.4",
     author="Theo Hanon",
     author_email="theo.hanon@student.uclouvain.be",
     description="A package for spherical positional encoding",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/TheoHanon/Spherical-Implicit-Neural-Representation",  
+    url="https://github.com/TheoHanon/Spherical-Implicit-Neural-Representation",
     packages=find_packages(include=["spherical_inr", "spherical_inr.*"]),
     install_requires=[
         "torch>=1.7.0",
--- spherical_inr/__init__.py
+++ spherical_inr/__init__.py
@@ -12,7 +12,7 @@ Modules:
     transforms: Coordinate transformation utilities.
 """
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 from .activations import *
 from .inr import *
