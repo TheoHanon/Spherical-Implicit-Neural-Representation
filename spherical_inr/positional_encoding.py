Fix normalization terms in sine and exponential calculations in Positional Encoding classes

spherical_inr/positional_encoding.py
@@ -290,8 +290,8 @@ class NormalizedRegularHerglotzPE(_PositionalEncoding):
         ax_R = ax.real
         ax_I = ax.imag
 
-        sin_term = torch.sin(self.w_R * (ax_I / self.rref)   + self.w_I * (ax_R / self.rref)  + self.b_I)
-        exp_term = torch.exp(self.w_R * ((ax_R / self.rref) - 1/math.sqrt(2.)) - self.w_I * ((ax_I/self.rref) + 1/math.sqrt(2.)) + self.b_R)
+        sin_term = torch.sin(self.w_R * (ax_I / self.rref - 1/math.sqrt(2.))  + self.w_I * (ax_R / self.rref - 1/math.sqrt(2.))  + self.b_I)
+        exp_term = torch.exp(self.w_R * ((ax_R / self.rref) - 1/math.sqrt(2.)) - self.w_I * ((ax_I/self.rref) - 1/math.sqrt(2.)) + self.b_R)
 
         return sin_term * exp_term
     
@@ -370,8 +370,8 @@ class NormalizedIrregularHerglotzPE(NormalizedRegularHerglotzPE):
         
             ax_R = ax.real
             ax_I = ax.imag
-            sin_term = torch.sin(self.w_R * (ax_I / r) * (self.rref/r) + self.w_I * (ax_R / r) * (self.rref/r) +  self.b_I)
-            exp_term = torch.exp(self.w_R * ( (ax_R / r) * (self.rref/r) - 1/math.sqrt(2.) ) - self.w_I * ( ( ax_I / r) * (self.rref/r) + 1/math.sqrt(2.) ) + self.b_R)
+            sin_term = torch.sin(self.w_R * ((ax_I / r) * (self.rref/r) - 1/math.sqrt(2.)) + self.w_I * ((ax_R / r) * (self.rref/r) - 1/math.sqrt(2)) +  self.b_I)
+            exp_term = torch.exp(self.w_R * ( (ax_R / r) * (self.rref/r) - 1/math.sqrt(2.) ) - self.w_I * ( ( ax_I / r) * (self.rref/r) - 1/math.sqrt(2.) ) + self.b_R)
 
             return  (1/r) * exp_term * sin_term 
 